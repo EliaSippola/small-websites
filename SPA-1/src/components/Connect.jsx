baca4f9a6dc98ba81d9722138a92e07e71dec103
Sippola Elia 16524 <elia.sippola@edu.riveria.fi>
2025-01-10 09:55:46 +0200

    changed SPA-1 to use function based components

--- SPA-1/src/components/Connect.jsx
+++ SPA-1/src/components/Connect.jsx
@@ -1,16 +1,8 @@
-import { Component } from "react";
-
-class Connect extends Component {
-
-    render() {
-        return (
-            <div>
-                <h2>Conntections</h2>
-                <p>easiest way to send email to a <a href="https://www.google.com">forum</a></p>
-            </div>
-        )
-    }
-
-}
-
-export default Connect;
+export default function Connect() {
+    return (
+        <div>
+            <h2>Conntections</h2>
+            <p>easiest way to send email to a <a href="https://www.google.com">forum</a></p>
+        </div>
+    )
+};
--- SPA-1/src/components/Story.jsx
+++ SPA-1/src/components/Story.jsx
@@ -1,24 +1,16 @@
-import { Component } from "react";
-
-class Story extends Component {
-
-    render() {
-        return (
-            <div>
-                <h2>Story page</h2>
-                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio facere, earum excepturi ea molestiae suscipit aliquam error iusto, quidem dignissimos recusandae aut inventore animi exercitationem, assumenda ipsum blanditiis? Optio, repudiandae.</p>
-                <ol>
-                    <li>List point 1</li>
-                    <li>List point 2</li>
-                    <li>List point 3</li>
-                    <li>List point 4</li>
-                    <li>List point 5</li>
-                    <li>List point 6</li>
-                </ol>
-            </div>
-        )
-    }
-
-}
-
-export default Story;
+export default function Story() {
+    return (
+        <div>
+            <h2>Story page</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Distinctio facere, earum excepturi ea molestiae suscipit aliquam error iusto, quidem dignissimos recusandae aut inventore animi exercitationem, assumenda ipsum blanditiis? Optio, repudiandae.</p>
+            <ol>
+                <li>List point 1</li>
+                <li>List point 2</li>
+                <li>List point 3</li>
+                <li>List point 4</li>
+                <li>List point 5</li>
+                <li>List point 6</li>
+            </ol>
+        </div>
+    )
+};
